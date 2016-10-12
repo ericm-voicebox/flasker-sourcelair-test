bf209de451770a6123ed91df755e1620084f9116
maingi <ericm@voicebox.com>
2016-10-12 22:21:19 +0300

    flasker project setup
    initial project created using sourcelair.com platform.

---
+++ server.py
@@ -0,0 +1,21 @@
+import os
+import glob
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/files')
+def get_files():
+    files = [f for f in glob.glob('*.*')]
+    return '<br/>'.join(files)
+
+if __name__ == '__main__':
+    try:
+        port = int(os.environ.get('PORT'))
+    except ValueError:
+        port = 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
